1ca498a147c2c16c1526c6726235213757d38e88
Mamal Maleki <mohammadalimalekie@gmail.com>
2025-02-24 19:16:32 +0330

    Add order and order status

--- NickiEatsOrderService.Domain/Entities/Order.cs
+++ NickiEatsOrderService.Domain/Entities/Order.cs
@@ -1,6 +1,39 @@
+using NickiEatsOrderService.Domain.Enums;
+
 namespace NickiEatsOrderService.Domain.Entities;
 
 public class Order
 {
-    
+    public Guid Id { get; private set; }
+    public DateTime OrderDate { get; private set; }
+    public List<OrderItem> Items { get; private set; } = new List<OrderItem>();
+    public Payment Payment { get; private set; }
+    public OrderStatus Status { get; private set; }
+
+    public Order(Guid id, DateTime orderDate)
+    {
+        Id = id;
+        OrderDate = orderDate;
+        Status = OrderStatus.Created;
+    }
+
+    public void AddItem(OrderItem item)
+    {
+        Items.Add(item);
+    }
+
+    public void SetPayment(Payment payment)
+    {
+        Payment = payment;
+    }
+
+    public void MarkAsCompleted()
+    {
+        Status = OrderStatus.Completed;
+    }
 }
+
+
+
+
+
--- NickiEatsOrderService.Domain/Enums/OrderStatus.cs
+++ NickiEatsOrderService.Domain/Enums/OrderStatus.cs
@@ -2,5 +2,8 @@ namespace NickiEatsOrderService.Domain.Enums;
 
 public enum OrderStatus
 {
-    
+    Created,
+    Processing,
+    Completed,
+    Cancelled
 }
