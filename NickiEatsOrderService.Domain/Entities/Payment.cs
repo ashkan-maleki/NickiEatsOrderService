eaecc9e9b0aa688f5396af73477d57e3d7610e18
Mamal Maleki <mohammadalimalekie@gmail.com>
2025-02-25 17:24:43 +0330

    add payment

--- NickiEatsOrderService.Domain/Entities/Payment.cs
+++ NickiEatsOrderService.Domain/Entities/Payment.cs
@@ -1,6 +1,24 @@
+using NickiEatsOrderService.Domain.Enums;
+
 namespace NickiEatsOrderService.Domain.Entities;
 
 public class Payment
 {
-    
+    public Guid Id { get; private set; }
+    public decimal Amount { get; private set; }
+    public PaymentStatus Status { get; private set; }
+    public DateTime? PaymentDate { get; private set; }
+
+    public Payment(Guid id, decimal amount)
+    {
+        Id = id;
+        Amount = amount;
+        Status = PaymentStatus.Pending;
+    }
+
+    public void CompletePayment()
+    {
+        Status = PaymentStatus.Completed;
+        PaymentDate = DateTime.UtcNow;
+    }
 }
