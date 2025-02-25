7c64d48b6658a1405f0c80fc39a1c6b621c51a52
Mamal Maleki <mohammadalimalekie@gmail.com>
2025-02-25 17:24:35 +0330

    Add order items

--- NickiEatsOrderService.Domain/Entities/OrderItem.cs
+++ NickiEatsOrderService.Domain/Entities/OrderItem.cs
@@ -2,5 +2,17 @@ namespace NickiEatsOrderService.Domain.Entities;
 
 public class OrderItem
 {
-    
+    public Guid Id { get; private set; }
+    public Guid ProductId { get; private set; }
+    public int Quantity { get; private set; }
+    public decimal UnitPrice { get; private set; }
+    public decimal TotalPrice => UnitPrice * Quantity;
+
+    public OrderItem(Guid id, Guid productId, int quantity, decimal unitPrice)
+    {
+        Id = id;
+        ProductId = productId;
+        Quantity = quantity;
+        UnitPrice = unitPrice;
+    }
 }
